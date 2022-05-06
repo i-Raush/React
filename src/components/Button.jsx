e2d9f29ecf741547e6d06bf88a9b93a634af3563
i-Raush <kumarraushanraj.it19@aryacollege.in>
2022-05-06 22:46:12 +0530

    friday commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,20 @@
+import {React, useState} from 'react'
+import style from './Button.module.css'
+
+const Button = () => {
+
+    let [text, setText] = useState('?')
+    const onTest = () => {
+        console.log("what's up!");
+        text = "I'm bored!"
+        console.log(text);
+        setText ('Nice')
+    }
+
+    return (<div>
+        <button className={style.button} onClick={onTest} >Submit</button>
+        <h1>{text}</h1>
+        </div>)
+}
+
+export default Button 
--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,8 +1,12 @@
 import style from './Demo.module.css';
 import Test from './Test'
+import Button from './Button'
 const Demo = () => {
-    return <div> <h1 className={style.blue}>All Good..?</h1>
-    <Test></Test> </div>
+    return <div> 
+        <h1 className={style.blue}>All Good..?</h1>
+    <Test></Test> 
+    <Button></Button>
+           </div>
 }
 
 export default Demo
