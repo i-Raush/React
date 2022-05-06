6d6d26ca6e4d0f18ea6a24acee399c774773abe7
i-Raush <kumarraushanraj.it19@aryacollege.in>
2022-05-06 07:20:10 +0530

    myReact

---
+++ src/components/Demo.jsx
@@ -0,0 +1,8 @@
+import style from './Demo.module.css';
+import Test from './Test'
+const Demo = () => {
+    return <div> <h1 className={style.blue}>All Good..?</h1>
+    <Test></Test> </div>
+}
+
+export default Demo
---
+++ src/components/Test.jsx
@@ -0,0 +1,7 @@
+import style from './Test.module.css'
+
+const Test = () => {
+    return <h1 className={style.green}>!...Hope You all are doing great...!</h1>
+}
+
+export default Test
